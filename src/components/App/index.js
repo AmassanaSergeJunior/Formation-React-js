9fd1b6d5fade8e131dee80cc637c921c47c994b5
AmassanaSergeJunior <amassanasergejunior@gmail.com>
2024-08-07 09:52:54 +0100

    Mettre en pratique les connaissances React dans un vrai proje

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header/index';
+import Landing from '../Landing/index';
+import Footer from '../Footer/index';
+import '../../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Landing/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer>
+      <div className='footer-container'>
+         <p> Projet realise par Amassana - 2024</p>
+         <p> Les icons Wolverine, Iron-man, Spider-man, batman sont prises sur iconFinder.com</p>
+      </div>
+   </footer>
+  )
+}
+
+export default Footer
